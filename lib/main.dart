56499c11282ec9553b036231ceb2e53b5bade219
muhamistau <islam.taufikurahman@gmail.com>
2019-06-24 14:31:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_web/material.dart';
+import 'package:my_portfolio/profile_page.dart';
+
+main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        fontFamily: "GoogleSansRegular",
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
